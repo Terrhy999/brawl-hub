95ddf4c270298a3cbc80a5d6125e57ac2b40f0e8
Terrhy999 <thyla999@gmail.com>
2023-09-20 10:00:56 -0400

    aetherhub, sort decklists by newest to oldest

--- src/main.rs
+++ src/main.rs
@@ -9,16 +9,27 @@ async fn main() -> WebDriverResult<()> {
         .goto("https://aetherhub.com/Decks/Historic-Brawl/")
         .await?;
 
+    // Select the group of buttons that let you view decks by time period
     let time_button_group = driver
         .find(By::Css("[aria-label=\"Toolbar with button groups\"]"))
         .await?
         .find_all(By::Tag("label"))
         .await?;
 
+    // Select, then click, the 'year' button to view all decks in the past year
     let year_button = &time_button_group[2];
     year_button.click().await?;
 
-    println!("{}", year_button.outer_html().await?);
+    // Select the table containing the decklists
+    let meta_hub_table = driver.find(By::Id("metaHubTable")).await?;
+
+    // Select, then click, the decklist table heading "Time since last updated" to sort decks by newest to oldest
+    let meta_hub_table_heading_time = meta_hub_table
+        .find(By::Tag("thead"))
+        .await?
+        .find(By::Css("[title=\"Time since last updated\"]"))
+        .await?;
+    meta_hub_table_heading_time.click().await?;
 
     driver.quit().await?;
     Ok(())
