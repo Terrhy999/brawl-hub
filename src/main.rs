ff59239b22cc25743d90c2db0f540cd828cd669e
Terrhy999 <thyla999@gmail.com>
2023-09-21 09:19:47 -0400

    Collect decklist URLs on the current page

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use futures::future::join_all;
 use thirtyfour::prelude::*;
 
 #[tokio::main]
@@ -30,6 +31,31 @@ async fn main() -> WebDriverResult<()> {
         .find(By::Css("[title=\"Time since last updated\"]"))
         .await?;
     meta_hub_table_heading_time.click().await?;
+
+    // A Vec of WebElements for each row in the decklist table on the current page
+    let meta_hub_table_rows = meta_hub_table
+        .find(By::Tag("tbody"))
+        .await?
+        .find_all(By::Tag("tr"))
+        .await?;
+
+    // Collect into a Vec the first 'a' element in each row, which is the decklist url
+    let table_decklist_url_futures = meta_hub_table_rows
+        .iter()
+        .map(|x| async {
+            let a_tag = x.find(By::Tag("a")).await.unwrap();
+            println!("{:#?}", a_tag.outer_html().await);
+            let href = a_tag.attr("href").await.unwrap();
+            match href {
+                None => String::from("whoops no url"),
+                Some(url) => url,
+            }
+        })
+        .collect::<Vec<_>>();
+
+    // Squish the Vec of futures into one future
+    let table_decklist_urls = join_all(table_decklist_url_futures).await;
+    println!("{:#?}", table_decklist_urls);
 
     driver.quit().await?;
     Ok(())
