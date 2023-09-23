51d3622f2a504765133296b80b866c93956821bc
Terrhy999 <thyla999@gmail.com>
2023-09-23 10:14:58 -0400

    expect Results instead of using '?', started loop through decklist pages

--- src/main.rs
+++ src/main.rs
@@ -22,40 +22,81 @@ async fn main() -> WebDriverResult<()> {
     year_button.click().await?;
 
     // Select the table containing the decklists
-    let meta_hub_table = driver.find(By::Id("metaHubTable")).await?;
+    let meta_hub_table = driver
+        .find(By::Id("metaHubTable"))
+        .await
+        .expect("Couldn't select meta_hub_table");
 
     // Select, then click, the decklist table heading "Time since last updated" to sort decks by newest to oldest
     let meta_hub_table_heading_time = meta_hub_table
         .find(By::Tag("thead"))
-        .await?
+        .await
+        .expect("couldn't find table header")
         .find(By::Css("[title=\"Time since last updated\"]"))
-        .await?;
-    meta_hub_table_heading_time.click().await?;
+        .await
+        .expect("couldn't find the 'time' button");
 
-    // A Vec of WebElements for each row in the decklist table on the current page
-    let meta_hub_table_rows = meta_hub_table
-        .find(By::Tag("tbody"))
-        .await?
-        .find_all(By::Tag("tr"))
-        .await?;
+    meta_hub_table_heading_time
+        .click()
+        .await
+        .expect("couldn't click the 'time' button");
 
-    // Collect into a Vec the first 'a' element in each row, which is the decklist url
-    let table_decklist_url_futures = meta_hub_table_rows
-        .iter()
-        .map(|x| async {
-            let a_tag = x.find(By::Tag("a")).await.unwrap();
-            println!("{:#?}", a_tag.outer_html().await);
-            let href = a_tag.attr("href").await.unwrap();
-            match href {
-                None => String::from("whoops no url"),
-                Some(url) => url,
-            }
-        })
-        .collect::<Vec<_>>();
+    // meta_hub_table_paginate_next
+    //     .wait_until()
+    //     .wait(3, 0)
+    //     .await
+    //     .expect("timed out waiting for 'next page' button to have 'disabled' class");
 
-    // Squish the Vec of futures into one future
-    let table_decklist_urls = join_all(table_decklist_url_futures).await;
-    println!("{:#?}", table_decklist_urls);
+    // let paginate_next_is_disabled = match meta_hub_table_paginate_next
+    //     .class_name()
+    //     .await
+    //     .expect("couldn't get 'next page' button classname")
+    // {
+    //     Some(class_name) => class_name.contains("disabled"),
+    //     None => false,
+    // };
+
+    println!("Got to the loop");
+
+    while true {
+        // A Vec of WebElements for each row in the decklist table on the current page
+        let meta_hub_table_rows = meta_hub_table
+            .find(By::Tag("tbody"))
+            .await?
+            .find_all(By::Tag("tr"))
+            .await?;
+
+        // Collect into a Vec the first 'a' element in each row, which is the decklist url
+        let table_decklist_url_futures = meta_hub_table_rows
+            .iter()
+            .map(|x| async {
+                let a_tag = x.find(By::Tag("a")).await.expect("couldn't find 'a' tag");
+
+                let href = a_tag
+                    .attr("href")
+                    .await
+                    .expect("no 'href' attribute on 'a' tag");
+                match href {
+                    None => String::from("whoops no url"),
+                    Some(url) => url,
+                }
+            })
+            .collect::<Vec<_>>();
+
+        // Squish the Vec of futures into one future
+        let table_decklist_urls = join_all(table_decklist_url_futures).await;
+        // println!("{:#?}", table_decklist_urls);
+
+        let meta_hub_table_paginate_next = driver
+            .find(By::Id("metaHubTable_next"))
+            .await
+            .expect("could't find 'next page' button");
+
+        meta_hub_table_paginate_next
+            .click()
+            .await
+            .expect("couldn't click 'next page' button");
+    }
 
     driver.quit().await?;
     Ok(())
