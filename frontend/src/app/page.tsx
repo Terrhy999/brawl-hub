4d22ce60233404efed538c9ab11954d7d0763646
Terrhy999 <thyla999@gmail.com>
2023-10-26 18:30:48 -0400

    Removed README, setup Axum get route

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,5 +1,20 @@
-import Image from "next/image";
+async function getTopCommanders() {
+  const res = await fetch("http://localhost:3030");
+  return res.json();
+}
 
-export default function Home() {
-  return <main className=""></main>;
+export default async function Home() {
+  const top_commanders = await getTopCommanders();
+  console.log(top_commanders);
+  return (
+    <main>
+      {top_commanders.map(
+        (c: { commander: string; count: number }, i: number) => (
+          <div key={i}>
+            {c.count} of {c.commander}
+          </div>
+        )
+      )}
+    </main>
+  );
 }
--- server/src/main.rs
+++ server/src/main.rs
@@ -1,28 +1,42 @@
-use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use axum::{debug_handler, routing::get, Json, Router};
+use sqlx::{postgres::PgPoolOptions, Pool, Postgres};
+use std::net::SocketAddr;
 
-#[actix_web::main]
-async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
-        App::new()
-            .service(hello)
-            .service(echo)
-            .route("/hey", web::get().to(manual_hello))
-    })
-    .bind(("127.0.0.1", 8080))?
-    .run()
+const DATABASE_URL: &str = "postgres://postgres:postgres@localhost/brawlhub";
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/", get(top_commanders));
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3030));
+    println!("Server listening on {addr}");
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .expect("Failed to start server");
+}
+
+#[derive(serde::Serialize)]
+struct Commander {
+    commander: String,
+    count: Option<i64>,
+}
+
+#[debug_handler]
+async fn top_commanders() -> Json<Vec<Commander>> {
+    let pool = PgPoolOptions::new()
+        .max_connections(5)
+        .connect(DATABASE_URL)
+        .await
+        .expect("Couldn't connect to db");
+
+    let res = sqlx::query_as!(
+        Commander,
+        // "SELECT c.name as commander FROM deck d JOIN card c ON d.commander = c.oracle_id"
+        "SELECT c.name as commander, COUNT(commander) AS count FROM deck d JOIN card c ON d.commander = c.oracle_id GROUP BY c.name ORDER BY count DESC LIMIT 100"
+    )
+    .fetch_all(&pool)
     .await
-}
+    .expect("error retrieving from db");
 
-#[get("/")]
-async fn hello() -> impl Responder {
-    HttpResponse::Ok().body("Hello world!")
-}
-
-#[post("/echo")]
-async fn echo(req_body: String) -> impl Responder {
-    HttpResponse::Ok().body(req_body)
-}
-
-async fn manual_hello() -> impl Responder {
-    HttpResponse::Ok().body("Hey there!")
+    Json(res)
 }
