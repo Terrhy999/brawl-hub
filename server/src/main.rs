8d1b612db5636fc2c17445c60072c9c67a164c29
chrid97 <cgoncal12@gmail.com>
2023-10-28 12:14:09 -0400

    Add Endpoint to get commanders by color

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,4 +1,4 @@
-use axum::{debug_handler, routing::get, Json, Router};
+use axum::{debug_handler, extract::Path, routing::get, Json, Router};
 use sqlx::postgres::PgPoolOptions;
 use std::net::SocketAddr;
 
@@ -8,7 +8,8 @@ const DATABASE_URL: &str = "postgres://postgres:postgres@localhost/brawlhub";
 async fn main() {
     let app = Router::new()
         .route("/get_top_commanders", get(top_commanders))
-        .route("/get_top_cards", get(top_cards));
+        .route("/get_top_cards", get(top_cards))
+        .route("/commanders/:colors", get(get_commanders_of_color));
     let addr = SocketAddr::from(([127, 0, 0, 1], 3030));
     println!("Server listening on {addr}");
     axum::Server::bind(&addr)
@@ -39,6 +40,58 @@ struct CommanderInfo {
     image_art_crop: String,
     image_border_crop: String,
     count: Option<i64>,
+}
+
+#[derive(serde::Serialize)]
+struct Card {
+    oracle_id: String,
+    name: String,
+    lang: String,
+    scryfall_uri: String,
+    layout: String,
+    mana_cost: Option<String>,
+    cmc: f32,
+    type_line: String,
+    oracle_text: Option<String>,
+    colors: Option<Vec<String>>,
+    color_identity: Vec<String>,
+    is_legal: bool,
+    is_commander: bool,
+    rarity: String,
+    image_small: String,
+    image_normal: String,
+    image_large: String,
+    image_art_crop: String,
+    image_border_crop: String,
+}
+
+async fn get_commanders_of_color(Path(id): Path<String>) -> Json<Vec<Card>> {
+    // Have to figure out how handle routes that aren't wubrg or colorless
+    let mut colors = vec![];
+    if id == "colorless" {
+    } else {
+        for char in id.chars() {
+            colors.push(char.to_uppercase().to_string());
+        }
+    }
+
+    // println!("{:#?}", colors);
+    let pool = PgPoolOptions::new()
+        .max_connections(5)
+        .connect(DATABASE_URL)
+        .await
+        .expect("Couldn't connect to db");
+
+    let res = sqlx::query_as!(
+        Card,
+        "SELECT * FROM card WHERE is_commander = true AND color_identity <@ $1::char(1)[] AND color_identity @> $1::char(1)[];",
+        &colors
+    )
+    .fetch_all(&pool)
+    .await
+    .expect("error retrieving from db");
+
+    Json(res)
 }
 
 #[debug_handler]
