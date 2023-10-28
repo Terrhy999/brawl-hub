07ad7777d6f331bce74df04efcfadccf713c2b6b
chrid97 <cgoncal12@gmail.com>
2023-10-28 12:46:06 -0400

    Add mana symbols

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 
 async function getTopCommanders() {
     const res = await fetch("http://127.0.0.1:3030/get_top_commanders", {
+    // const res = await fetch("http://127.0.0.1:3030/get_top_commanders", {
         cache: "no-cache",
     });
     return res.json();
@@ -38,7 +39,7 @@ type Card = {
 };
 
 export default async function Home() {
-    const activeDateFilter: 'year' | 'month' | 'week'  = 'year';
+    let activeDateFilter = 'year';
     const top_commanders: Card[] = await getTopCommanders();
     // const top_cards: TopCard[] = await getTopCards();
     // console.log(top_commanders);
@@ -48,11 +49,23 @@ export default async function Home() {
             <NavBar />
             <main className="m-10">
                 <h1 className="text-[32px]">Top Commanders</h1>
-                <span className="flex [&>*]:mr-[12px]">
-                    <ClickableChip text={"Year"} isActive={activeDateFilter === 'year'} />
-                    <ClickableChip text={"Month"} />
-                    <ClickableChip text={"Week"} />
-                </span>
+                <div className="flex justify-between mb-5">
+                    {/* Change this to a radio button */}
+                    <span className="flex [&>*]:mr-[12px]">
+                        <ClickableChip text={"Year"} isActive={activeDateFilter === "year"} />
+                        <ClickableChip text={"Month"} isActive={activeDateFilter === "month"} />
+                        <ClickableChip text={"Week"} isActive={activeDateFilter === "week"} />
+                    </span>
+
+                    <div className="flex [&>*]:mr-[20px] [&>*]:opacity-30 [&>*]:duration-[0.3s]">
+                        <Image src={"/white-mana-symbol.png"} alt={"Green mana"} width={36} height={36} />
+                        <Image src={"/blue-mana-symbol.png"} alt={"Green mana"} width={36} height={36} />
+                        <Image src={"/black-mana-symbol.png"} alt={"Green mana"} width={36} height={36} />
+                        <Image src={"/red-mana-symbol.png"} alt={"Green mana"} width={36} height={36} />
+                        <Image src={"/green-mana-symbol.png"} alt={"Green mana"} width={36} height={36} />
+                    </div>
+                </div>
+
                 <div className="grid gap-[20px] grid-cols-[repeat(auto-fit,minmax(270px,1fr))]">
                     {top_commanders.map((c, i: number) => (
                         <CardAndRank key={i} i={i} c={c} />
@@ -63,10 +76,25 @@ export default async function Home() {
     );
 }
 
-function ClickableChip({ text, isActive = false }: { text: string, isActive?: boolean }) {
-    const activeClass = isActive ? "bg-[rgb(241,241,241)] text-[rgb(15,15,15)] " : "";
+function ClickableChip({
+    text,
+    isActive = false,
+    onClick = undefined,
+}: {
+    text: string;
+    isActive?: boolean;
+    onClick?: () => void | undefined;
+}) {
+    const activeClass = isActive
+        ? "bg-[rgb(241,241,241)] text-[rgb(15,15,15)] "
+        : "";
     return (
-        <button className={`rounded-[8px] bg-white/[0.1] h-[32px] w-m-[12px] px-[12px] font-medium flex items-center ${activeClass}`}>{text}</button>
+        <button
+            onClick={onClick}
+            className={`rounded-[8px] bg-white/[0.1] h-[32px] w-m-[12px] px-[12px] font-medium flex items-center ${activeClass}`}
+        >
+            {text}
+        </button>
     );
 }
 
@@ -76,13 +104,19 @@ function NavBar() {
             BrawlRec
             <nav></nav>
         </header>
-    )
+    );
 }
 
-function CardAndRank({ i, c }: { i: number, c: Card }) {
+function CardAndRank({ i, c }: { i: number; c: Card }) {
     return (
         <div className="flex flex-col items-center">
-            <Image className="rounded-[5%] max-h-[340px]" src={c.image_large} alt={c.name} width={244} height={340} />
+            <Image
+                className="rounded-[5%] max-h-[340px]"
+                src={c.image_large}
+                alt={c.name}
+                width={244}
+                height={340}
+            />
             <div className="text-center">
                 <div>Rank #{i + 1}</div>
                 <div>{c.count} decks</div>
