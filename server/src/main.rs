328eab5ade9d7ae84bc59f46df34253006e0cf02
chrid97 <cgoncal12@gmail.com>
2023-10-29 19:36:06 -0400

    Add endpoint for retrieving all legal historic brawl commanders

--- server/src/main.rs
+++ server/src/main.rs
@@ -9,7 +9,8 @@ async fn main() {
     let app = Router::new()
         .route("/get_top_commanders", get(top_commanders))
         .route("/get_top_cards", get(top_cards))
-        .route("/commanders/:colors", get(get_commanders_of_color));
+        .route("/commanders/:colors", get(get_commanders_of_color))
+        .route("/all_commanders", get(get_historic_brawl_commanders));
     let addr = SocketAddr::from(([127, 0, 0, 1], 3030));
     println!("Server listening on {addr}");
     axum::Server::bind(&addr)
@@ -40,6 +41,47 @@ struct CommanderInfo {
     image_art_crop: String,
     image_border_crop: String,
     count: Option<i64>,
+}
+
+#[derive(serde::Serialize)]
+struct Commander {
+    oracle_id: String,
+    name: String,
+    lang: String,
+    scryfall_uri: String,
+    layout: String,
+    mana_cost: Option<String>,
+    cmc: f32,
+    type_line: String,
+    oracle_text: Option<String>,
+    colors: Option<Vec<String>>,
+    color_identity: Vec<String>,
+    is_legal: bool,
+    is_commander: bool,
+    rarity: String,
+    image_small: String,
+    image_normal: String,
+    image_large: String,
+    image_art_crop: String,
+    image_border_crop: String,
+}
+
+async fn get_historic_brawl_commanders()-> Json<Vec<Commander>> {
+    let pool = PgPoolOptions::new()
+        .max_connections(5)
+        .connect(DATABASE_URL)
+        .await
+        .expect("Couldn't connect to db");
+
+    let res = sqlx::query_as!(
+        Commander,
+        "SELECT * FROM card WHERE is_commander = true AND is_legal",
+    )
+    .fetch_all(&pool)
+    .await
+    .expect("error retrieving from db");
+
+    Json(res)
 }
 
 async fn get_commanders_of_color(Path(id): Path<String>) -> Json<Vec<CommanderInfo>> {
