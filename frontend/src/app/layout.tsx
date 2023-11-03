c290a7ad098f0bf368678d31f733dc2afa03df65
chrid97 <cgoncal12@gmail.com>
2023-11-03 16:32:32 -0400

    Make header responsive and add hamburger menu

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -27,15 +27,63 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
 
 function NavBar() {
   return (
-    <header className="h-[--header-height] bg-header-color sticky top-0 z-50">
-      <div className="max-w-[85%] mx-auto flex items-center p-4 justify-between">
-        <span>BrawlRec</span>
-        <nav className="[&>*]:mr-5">
-          <Link href={'/commanders/'}>Top Commanders</Link>
-          <Link href={'/cards/'}>Top Cards</Link>
-        </nav>
+    <header className="md:h-[--header-height] bg-header-color sticky top-0 z-50">
+      <div className="max-w-[85%] mx-auto flex py-4 whitespace-nowrap items-center">
+        <span className="hidden md:block">BrawlHub</span>
+        <span className="md:hidden">BH</span>
         <SearchBar />
+        <div className="[border-right:1px_solid_rgba(255,255,255,0.4)] mr-[6px] h-[20px] hidden md:block" />
+        <nav className="[&>*]:px-[7px] hidden md:block">
+          <Link href={'/commanders/'} className="!pl-[14px]">
+            Commanders
+          </Link>
+          <Link href={'/cards/'}>Cards</Link>
+          <Link href={''}>Random</Link>
+        </nav>
+        <BarsSvg className="md:hidden"></BarsSvg>
       </div>
+      <nav className="max-w-[85%] mx-auto [&>*]:w-[49%] [&>*]:mb-[8px] flex justify-between flex-wrap [border-top:1px_solid_rgba(255,255,255,0.4)] pt-[12px] pb-[8px] md:hidden">
+        <Link
+          href={'/commanders/'}
+          className="[border:1px_solid_rgba(255,255,255,0.1)] px-[10px] py-[1px] rounded-[4px] bg-[color:rgba(255,255,255,.1)]"
+        >
+          Commanders
+        </Link>
+        <Link
+          href={'/cards/'}
+          className="[border:1px_solid_rgba(255,255,255,0.1)] px-[10px] py-[1px] rounded-[4px] bg-[color:rgba(255,255,255,.1)]"
+        >
+          Cards
+        </Link>
+        <Link
+          href={'/commanders/'}
+          className="[border:1px_solid_rgba(255,255,255,0.1)] px-[10px] py-[1px] rounded-[4px] bg-[color:rgba(255,255,255,.1)]"
+        >
+          Sets
+        </Link>
+        <Link
+          href={'/commanders/'}
+          className="[border:1px_solid_rgba(255,255,255,0.1)] px-[10px] py-[1px] rounded-[4px] bg-[color:rgba(255,255,255,.1)]"
+        >
+          Random
+        </Link>
+      </nav>
     </header>
+  )
+}
+
+function BarsSvg({ className }: { className: string }) {
+  return (
+    <svg
+      // onClick={}
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth="1.5"
+      stroke="currentColor"
+      className={`w-6 h-6 ${className}`}
+    >
+      <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+    </svg>
   )
 }
