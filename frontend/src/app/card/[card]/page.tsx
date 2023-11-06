a195ea6cd4b13e915f8f9056a1caf8fe206dd525
chrid97 <cgoncal12@gmail.com>
2023-11-06 15:24:42 -0500

    Add not found page for cards page. Missing a not foudn page was causing a build eror when card slugs were undefined

--- frontend/src/app/card/[card]/page.tsx
+++ frontend/src/app/card/[card]/page.tsx
@@ -3,19 +3,23 @@ import { Card } from '@/app/_components/card-grid'
 import Link from 'next/link'
 import React from 'react'
 import { fetchJsonFromBrawlhub } from '@/app/_utils/fetch-json'
+import NotFound from '../not-found'
 
 export const dynamicParams = false
 export async function generateStaticParams() {
   // (TODO) find out why this is undefined sometimes
-  return (await fetchJsonFromBrawlhub<string[]>(`card_slugs`)).map((name) => ({ card: name || '404' }))
+  return (await fetchJsonFromBrawlhub<string[]>(`card_slugs`)).map((name) => ({ card: name || 'not-found' }))
 }
 
 // async function getCommanderTopCards(oracle_id: string): Promise<TopCards> {
 //   return await fetch(`http://127.0.0.1:3030/top_cards_for_commander/${oracle_id}`).then((res) => res.json())
 // }
 
-export default async function Page({ params }: { params: { card: string } }) {
-  const cardSlug = params.card
+export default async function Page({ params = null }: { params: { card: string | 'not-found' } | null }) {
+  const cardSlug = params?.card
+  if (cardSlug || cardSlug === 'not-found') {
+    return <NotFound />
+  }
   const card = await fetchJsonFromBrawlhub<Card>(`card/${cardSlug}`)
   // const topCards = await getCommanderTopCards(commanderCard.oracle_id)
   return (
