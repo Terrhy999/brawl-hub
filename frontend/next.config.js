5d681831fa057e241545a29b059fa2c29962409c
chrid97 <cgoncal12@gmail.com>
2023-11-09 09:53:28 -0500

    Temporarily disable NextJS image optimization because we reached the free optimization limit, so our non cached images arent being served anymore

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,15 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        // formats: ['image/webp, image/jpg'],
-        remotePatterns: [
-            {
-                protocol: 'https',
-                hostname: 'cards.scryfall.io',
-                port: '',
-            }
-        ]
-    }
+  images: {
+    unoptimized: true,
+    // formats: ['image/webp, image/jpg'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'cards.scryfall.io',
+        port: '',
+      },
+    ],
+  },
 }
 
 module.exports = nextConfig
