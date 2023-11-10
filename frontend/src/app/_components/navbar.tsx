f5e159c97788f115a87ef1623287a49421fb508a
chrid97 <cgoncal12@gmail.com>
2023-11-10 16:39:57 -0500

    Add search to home page

--- frontend/src/app/_components/navbar.tsx
+++ frontend/src/app/_components/navbar.tsx
@@ -60,28 +60,6 @@ export function NavBar() {
   )
 }
 
-function BarsSvg({
-  className = '',
-  onClick = undefined,
-}: {
-  className?: string
-  onClick?: MouseEventHandler<SVGSVGElement> | undefined
-}) {
-  return (
-    <svg
-      onClick={onClick}
-      xmlns="http://www.w3.org/2000/svg"
-      fill="none"
-      viewBox="0 0 24 24"
-      strokeWidth="1.5"
-      stroke="currentColor"
-      className={`w-6 h-6 ${className}`}
-    >
-      <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
-    </svg>
-  )
-}
-
 type SearchResults = { cardName: string; image: string; slug: string }
 export function SearchBar() {
   const [searchQuery, setSearchQuery] = useState('')
@@ -111,7 +89,7 @@ export function SearchBar() {
         />
         {/* (TODO) Figure out how to get this div to be the COMPUTED width of parent */}
         {/* <div className="overflow-auto max-h-[300px] absolute bg-bg-color box-content rounded translate-y-[16px] w-[53%] [border:1px_solid_rgba(0,0,0,0.4)]"> */}
-        <div className="overflow-auto max-h-[300px] absolute bg-bg-color box-content rounded translate-y-[16px] w-[53%]">
+        <div className="overflow-auto max-h-[300px] absolute bg-bg-color rounded translate-y-[16px] ">
           {searchResults?.map((result, i) => {
             return (
               <Link href={`${result.slug}`} key={i} className="flex items-center">
@@ -151,6 +129,28 @@ function MagnifyingGlass({ className }: { className: string }) {
         strokeLinejoin="round"
         d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"
       />
+    </svg>
+  )
+}
+
+function BarsSvg({
+  className = '',
+  onClick = undefined,
+}: {
+  className?: string
+  onClick?: MouseEventHandler<SVGSVGElement> | undefined
+}) {
+  return (
+    <svg
+      onClick={onClick}
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth="1.5"
+      stroke="currentColor"
+      className={`w-6 h-6 ${className}`}
+    >
+      <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
     </svg>
   )
 }
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,12 +1,20 @@
+'use client'
 import Link from 'next/link'
+import { ChangeEvent, useEffect, useState } from 'react'
+import Image from 'next/image'
+import { fetchJsonFromPublic } from './_utils/fetch-json'
 
-export default async function Home() {
+export default function Home() {
   return (
     <div className="px-[5%] flex justify-center items-center h-screen flex-col [background:linear-gradient(to_top,#213B20,#1B341F,#162F1E,#132B1E,#10281D,#0A211C)]">
       <h1 className="text-[24px] md:text-[32px] text-center mb-[20px]">
         BrawlHub is the #1 spot to get rec&apos;s for your Historic Brawl decks!
       </h1>
-      <input className="[border:1px_solid_rgba(255,255,255,0.25)] bg-[#0A211C] py-[12px] pr-[14px] pl-[14px] w-full max-w-[524px] focus:outline-none"></input>
+      {/* <input
+        className="[border:1px_solid_rgba(255,255,255,0.25)] bg-[#0A211C] py-[12px] pr-[14px] pl-[14px] w-full max-w-[524px] focus:outline-none"
+        placeholder="Search for Magic cards..."
+      /> */}
+      <SearchBar />
       <nav className={`flex justify-between pt-[12px] mb-[200px]`}>
         <Link
           href={'/commanders/'}
@@ -23,4 +31,54 @@ export default async function Home() {
       </nav>
     </div>
   )
+}
+
+type SearchResults = { cardName: string; image: string; slug: string }
+export function SearchBar() {
+  const [searchQuery, setSearchQuery] = useState('')
+  const [searchResults, setSearchResults] = useState<SearchResults[]>()
+  useEffect(() => {
+    onSearch(searchQuery)
+  }, [searchQuery])
+
+  async function onSearch(searchQuery: string) {
+    if (!searchQuery) {
+      setSearchResults([])
+      return
+    }
+
+    const searchResults = await searchCard(searchQuery)
+    setSearchResults(searchResults)
+  }
+  return (
+    <>
+      <input
+        className="[border:1px_solid_rgba(255,255,255,0.25)] bg-[#0A211C] py-[12px] px-[14px] w-full max-w-[524px] focus:outline-none"
+        value={searchQuery}
+        onChange={(event: ChangeEvent<HTMLInputElement>) => setSearchQuery(event.target.value)}
+        placeholder="Search for Magic cards..."
+      />
+      {/* <div className="overflow-auto max-h-[200px] absolute bg-bg-color box-content rounded sm:translate-y-[26%] md:translate-y-[65px] lg:translate-y-[40px] w-full max-w-[524px]"> */}
+      <div className="overflow-auto max-h-[200px] rounded w-full max-w-[524px] bg-bg-color">
+        {searchResults?.map((result, i) => {
+          return (
+            <Link href={`${result.slug}`} key={i} className="flex items-center">
+              <Image
+                className="mr-2 h-[40px] w-[50px]"
+                src={result.image}
+                width={50}
+                height={50}
+                alt={result.cardName}
+              />
+              <div>{result.cardName}</div>
+            </Link>
+          )
+        })}
+      </div>
+    </>
+  )
+}
+
+async function searchCard(cardName: string) {
+  return await fetchJsonFromPublic<SearchResults[]>(`search/${cardName}`)
 }
