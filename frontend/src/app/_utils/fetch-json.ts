f1a1880a5b1236b1c36f13bbf6d7521dc1e4053b
Terrhy999 <thyla999@gmail.com>
2023-12-19 18:37:40 -0500

    fixed hardcoded URL

--- frontend/src/app/_utils/fetch-json.ts
+++ frontend/src/app/_utils/fetch-json.ts
@@ -3,7 +3,7 @@ export async function fetchJson<T>(url: RequestInfo, init?: RequestInit | undefi
 }
 
 export async function fetchJsonFromBrawlhub<T>(endpoint: string, init?: RequestInit | undefined): Promise<T> {
-  return (await fetch(`$https://api.brawlhub.net/${endpoint}`, init)).json()
+  return (await fetch(`https://api.brawlhub.net/${endpoint}`, init)).json()
 }
 
 export async function fetchJsonFromPublic<T>(endpoint: string, init?: RequestInit | undefined): Promise<T> {
